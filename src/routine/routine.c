519fdfd164c1ac0e5f622fbf18438e033466df21
Ahmad Nashwan <ahmad.naswan.07@gmail.com>
2026-05-16 16:13:28 +0300

    Simplify routine function and improve single philo handling, and bump usleep for even-id philos and thinking

--- src/routine/routine.c
+++ src/routine/routine.c
@@ -6,7 +6,7 @@
 /*   By: anashwan <anashwan@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 19:05:21 by anashwan          #+#    #+#             */
-/*   Updated: 2026/05/15 15:37:08 by anashwan         ###   ########.fr       */
+/*   Updated: 2026/05/16 16:04:17 by anashwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,13 @@ int	single_philo(t_philo *philo)
 {
 	if (philo->table->total == 1)
 	{
+		pthread_mutex_lock(philo->left_fork);
 		print_action(philo, "has taken a fork");
 		smart_sleep(philo->table->to_die, philo->table);
-		return (FAILURE);
+		pthread_mutex_unlock(philo->left_fork);
+		return (1);
 	}
-	return (OK);
+	return (0);
 }
 
 void	*routine(void *p)
@@ -40,24 +42,17 @@ void	*routine(void *p)
 	t_philo	*philo;
 
 	philo = (t_philo *)p;
-	if (single_philo(philo) != OK)
+	if (single_philo(philo))
 		return (NULL);
 	if (philo->id % 2 == 0)
-		usleep(500);
+		usleep(1000);
 	while (1)
 	{
 		if (check_simulation_end(philo->table))
 			return (NULL);
-		if (eating(philo))
-			return (NULL);
-		if (check_simulation_end(philo->table))
-			return (NULL);
-		if (sleeping(philo))
-			return (NULL);
-		if (check_simulation_end(philo->table))
-			return (NULL);
-		if (thinking(philo))
-			return (NULL);
+		if (eating(philo) == FAILURE || sleeping(philo) == FAILURE ||
+			thinking(philo) == FAILURE)
+				return (NULL);
 	}
 	return (NULL);
 }
--- src/routine/thinking.c
+++ src/routine/thinking.c
@@ -6,7 +6,7 @@
 /*   By: anashwan <anashwan@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 15:31:16 by anashwan          #+#    #+#             */
-/*   Updated: 2026/05/15 17:19:27 by anashwan         ###   ########.fr       */
+/*   Updated: 2026/05/16 15:54:20 by anashwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int	thinking(t_philo *philo)
 	if (check_simulation_end(philo->table))
 		return (FAILURE);
 	print_action(philo, "is thinking");
-	usleep(500);
+	usleep(1000);
 	return (OK);
 }
